820a6c18bde8f45c37119eed2fbdf4d8c960703d
Alejandro <faradius.magic187@gmail.com>
2023-04-20 16:42:08 -0600

    Mostrando la lista de proveedores en el recyclerView

--- app/src/main/java/com/developerscracks/sivapp/ui/proveedores/view/ProveedoresFragment.kt
+++ app/src/main/java/com/developerscracks/sivapp/ui/proveedores/view/ProveedoresFragment.kt
@@ -6,21 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.developerscracks.sivapp.R
 import com.developerscracks.sivapp.data.model.proveedor.Proveedor
 import com.developerscracks.sivapp.databinding.FragmentProveedoresBinding
+import com.developerscracks.sivapp.ui.proveedores.viewmodel.ProveedoresViewModel
 
 
 class ProveedoresFragment : Fragment(R.layout.fragment_proveedores), ProveedorAdapter.OnItemClickedProv {
 
     private lateinit var binding: FragmentProveedoresBinding
     private lateinit var adapter: ProveedorAdapter
+    private lateinit var viewmodel: ProveedoresViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProveedoresBinding.bind(view)
         (activity as AppCompatActivity).supportActionBar!!.title = "Proveedores"
+
+        viewmodel = ViewModelProvider(this)[ProveedoresViewModel::class.java]
+
+        setupRecyclerView()
+        viewmodel.getProveedores()
+
+        viewmodel.listaProveedores.observe(viewLifecycleOwner){
+            adapter.listaProveedores = it as ArrayList<Proveedor>
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun setupRecyclerView(){
