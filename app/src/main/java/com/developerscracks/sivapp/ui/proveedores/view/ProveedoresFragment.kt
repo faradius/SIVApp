222f0a6eb67095943ed6b6003aae000348a35f65
Alejandro <faradius.magic187@gmail.com>
2023-04-20 19:16:01 -0600

    Implementando el agregar proveedores

--- app/src/main/java/com/developerscracks/sivapp/ui/proveedores/view/ProveedoresFragment.kt
+++ app/src/main/java/com/developerscracks/sivapp/ui/proveedores/view/ProveedoresFragment.kt
@@ -1,5 +1,6 @@
 package com.developerscracks.sivapp.ui.proveedores.view
 
+import android.app.AlertDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
@@ -8,11 +9,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.developerscracks.sivapp.R
 import com.developerscracks.sivapp.data.model.proveedor.Proveedor
+import com.developerscracks.sivapp.databinding.AlertDialogProveedorBinding
 import com.developerscracks.sivapp.databinding.FragmentProveedoresBinding
 import com.developerscracks.sivapp.ui.proveedores.viewmodel.ProveedoresViewModel
 import es.dmoral.toasty.Toasty
@@ -21,6 +25,7 @@ import es.dmoral.toasty.Toasty
 class ProveedoresFragment : Fragment(R.layout.fragment_proveedores), ProveedorAdapter.OnItemClickedProv {
 
     private lateinit var binding: FragmentProveedoresBinding
+    private lateinit var bindingAlertDialog: AlertDialogProveedorBinding
     private lateinit var adapter: ProveedorAdapter
     private lateinit var viewmodel: ProveedoresViewModel
 
@@ -38,6 +43,52 @@ class ProveedoresFragment : Fragment(R.layout.fragment_proveedores), ProveedorAd
             adapter.listaProveedores = it as ArrayList<Proveedor>
             adapter.notifyDataSetChanged()
         }
+
+        viewmodel.mensaje.observe(viewLifecycleOwner){ mensaje ->
+            Toasty.info(requireContext(), mensaje, Toasty.LENGTH_SHORT, true).show()
+        }
+
+        binding.ibtnAdd.setOnClickListener {
+            alertDialogAddUpdate("add")
+        }
+    }
+
+    private fun alertDialogAddUpdate(accion:String){
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = requireActivity().layoutInflater
+        bindingAlertDialog = AlertDialogProveedorBinding.inflate(inflater)
+
+        builder.setView(bindingAlertDialog.root)
+
+        //Esto es para que no se cierre el alerdialog cuando se presiona fuera de el
+        builder.setCancelable(false)
+
+        if (accion == "add"){
+            builder.setTitle("Agregar Proveedor")
+        }
+
+        val etNomProveedor = bindingAlertDialog.etNomProveedor
+        val etTelefono = bindingAlertDialog.etTelefono
+        val etEmail = bindingAlertDialog.etEmail
+
+        builder.setPositiveButton("ACEPTAR"){_,_ ->
+
+                viewmodel.validarCampos(
+                    accion,
+                    etNomProveedor.text.toString().trim(),
+                    etTelefono.text.toString().trim(),
+                    etEmail.text.toString().trim()
+                )
+
+                adapter.notifyDataSetChanged()
+
+        }
+
+        builder.setNegativeButton("CANCELAR"){_,_ ->
+
+        }
+
+        builder.show()
     }
 
     private fun setupRecyclerView(){
@@ -72,6 +123,6 @@ class ProveedoresFragment : Fragment(R.layout.fragment_proveedores), ProveedorAd
     }
 
     override fun editarProveedor(prov: Proveedor) {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 }
